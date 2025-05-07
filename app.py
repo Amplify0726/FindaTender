9ee54f39a8da5659432381450ea4dff23f0e2387
Amplify0726 <tomrobinson06@gmail.com>
2025-05-07 21:36:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -90,8 +90,8 @@ def fetch_releases():
     to_date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     
     params = {
-        'publishedFrom': from_date,
-        'publishedTo': to_date,
+        'updatedFrom': from_date,
+        'updatedTo': to_date,
         'limit': 100
     }
     
